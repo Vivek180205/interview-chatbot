e0f270da322da62aea62431ef5c89ad52cee2809
Vivek <vivekrathod180205@gmail.com>
2026-04-28 03:09:51 +0530

    added simple Login Backend

---
+++ src/main/java/com/example/InterviewChatbot/models/User.java
@@ -0,0 +1,63 @@
+package com.example.InterviewChatbot.models;
+
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+
+public class User {
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+    private Date created_at;
+
+    public User() {}
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+}
