a305f3b41700fd31934b49dc02c6fd730feea620
Vivek <vivekrathod180205@gmail.com>
2026-04-27 14:18:05 +0530

    init commit

---
+++ src/main/java/com/example/InterviewChatbot/InterviewChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.example.InterviewChatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewChatbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/InterviewChatbot/InterviewChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.InterviewChatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
